12c58ed3a1a49a0c179a22e3352f09039382bcd6
paras <parassapariya212@gmail.com>
2023-11-10 13:20:48 +0530

    image uplord

--- index.js
+++ index.js
@@ -3,9 +3,47 @@ require('./connection');
 const model = require('./model');
 const { get } = require('http');
 
-const app = express();
 
+const app = express();
 app.use(express.json());
+
+
+app.listen(2000);
+
+
+//image uplord using api multer npm packege
+        // const multer = require('multer');
+
+        // const fileup = multer({
+        //     storage:multer.diskStorage({
+        //         destination:(req,file,cb)=>{
+        //             cb(null,"images");
+        //         },
+        //         filename:(res,file,cb)=>{
+        //             cb(null,file.fieldname+"-"+Date.now()+".jpg");
+        //         }
+        //     })
+        // }).single('user_file');
+
+
+        // app.post('/file',fileup,(req,res)=>{
+        //     res.send("done");
+        // })
+
+
+
+
+//api search post
+        // app.post('/:key', async (req,res)=>{
+        //     console.log(req.params.key);
+        //     let result = await model.find({
+        //         "$or":[
+        //             {"username":{$regex:req.params.key}},
+        //             {"password":{$regex:req.params.key}}
+        //         ]
+        //     });
+        //     res.send(result);
+        // });
 
 //api search 
         // app.get('/:key', async (req,res)=>{
@@ -18,20 +56,5 @@ app.use(express.json());
         //     });
         //     res.send(result);
         // });
-app.get('/',(req,res)=>{
-    res.send("done")
-});
-
-app.post('/:key', async (req,res)=>{
-    console.log(req.params.key);
-    let result = await model.find({
-        "$or":[
-            {"username":{$regex:req.params.key}},
-            {"password":{$regex:req.params.key}}
-        ]
-    });
-    res.send(result);
-});
 
 
-app.listen(2000);
